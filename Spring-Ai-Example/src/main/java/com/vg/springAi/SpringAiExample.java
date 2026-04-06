4e18b78571e009c078c9c1cedf92d96951ef112d
Vigin.Tse <844065343@qq.com>
2026-04-06 22:26:32 +0800

    依赖配置

---
+++ Spring-Ai-Example/src/main/java/com/vg/springAi/SpringAiExample.java
@@ -0,0 +1,12 @@
+package com.vg.springAi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiExample.class, args);
+    }
+}
